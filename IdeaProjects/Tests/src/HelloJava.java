0a292317dc4070f9739cfa037fecd82110ae9a47
Alexander Levanov <alevanov@web3tech.ru>
2026-04-30 08:02:40 +0300

    Let's do

--- IdeaProjects/Tests/src/HelloJava.java
+++ IdeaProjects/Tests/src/HelloJava.java
@@ -2,5 +2,6 @@ public class HelloJava {
     static void main() {
         System.out.println("Hello, world!");
         System.out.println("JavaRush");
+        System.out.println("Let's do physical exercises");
     }
 }
