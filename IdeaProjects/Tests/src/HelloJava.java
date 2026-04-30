4c522233ca92c5d557c1a8e754e96740773c6411
Alexander Levanov <alevanov@web3tech.ru>
2026-04-30 08:46:25 +0300

    transit to compact source and stucture fix

--- IdeaProjects/Tests/src/HelloJava.java
+++ IdeaProjects/Tests/src/HelloJava.java
@@ -1,7 +1,4 @@
-public class HelloJava {
-    static void main() {
+    void main() {
         System.out.println("Hello, world!");
-        System.out.println("JavaRush");
-        System.out.println("Let's do physical exercises");
+        System.out.println(LetsDo.LETS_DO);
     }
-}
---
+++ IdeaProjects/Tests/src/LetsDo.java
@@ -0,0 +1,4 @@
+public interface LetsDo {
+    public static final String LETS_DO = "Let's do physical exercises";
+
+}
